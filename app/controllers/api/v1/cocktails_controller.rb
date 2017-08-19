baacec8c1ec4dd8f4cfcc867e66badfc86633947
jeremylenz <jeremylenz@gmail.com>
2017-08-18 20:35:17 -0400

    im drunk

--- app/controllers/api/v1/cocktails_controller.rb
+++ app/controllers/api/v1/cocktails_controller.rb
@@ -1,19 +1,23 @@
-class API::V1::CocktailsController < ApplicationController
+class Api::V1::CocktailsController < ApplicationController
 
-  before_action :set_cocktail, except: [:index, :create]
+  before_action :set_cocktail, except: [:index, :create, :generate]
 
   def create
 
     # Here we will go to our datamuse API and get an awesome cocktail name based on the ingredients
+    # new_cocktail = Cocktail.create(cocktail_params)  # this will need to be changed
+    #
+    # if !new_cocktail.errors.empty?
+    #   render json: {status: "error", code: 400, message: new_cocktail.errors.full_messages[0]}, status: 400
+    # else
+    #   render json: new_cocktail
+    # end
+    byebug
+  end
 
-    new_cocktail = Cocktail.create(cocktail_params)  # this will need to be changed
-
-    if !new_cocktail.errors.empty?
-      render json: {status: "error", code: 400, message: new_cocktail.errors.full_messages[0]}, status: 400
-    else
-      render json: new_cocktail
-    end
-
+  def generate
+    new_cocktail_name =  generate_cocktail_name({"rum" => "2", "coke" => "3", "beef" => "1"})
+    render json: {name: new_cocktail_name}
   end
 
 
@@ -38,6 +42,7 @@ class API::V1::CocktailsController < ApplicationController
       render json: {status: "OK", code: 200, message: "Cocktail has been destroyed (with extreme prejudice)"}
     else
       render json: {status: "error", code: 500, message: "Couldn't destroy cocktail  ¯\\_(ツ)_/¯"}, status: 500
+    end
   end
 
   private
@@ -50,5 +55,31 @@ class API::V1::CocktailsController < ApplicationController
     params.require(:user).permit(:name, :is_alcoholic, :image_url)
   end
 
+  def generate_cocktail_name(data)
+    # data = {ingredient1: 2,
+  #        ingredient2: 2}
+    name_words = data.keys.map do |ingredient|
+      response = HTTParty.get("https://api.datamuse.com/words?rel_jjb=#{ingredient}&topics=alcohol")
+      !response.empty? ? response[rand(result_length(response.length))]["word"].capitalize : nil
+    end.compact
+    response_b = HTTParty.get("https://api.datamuse.com/words?rel_jja=#{data.keys.last}&topics=alcohol")
+    if !response_b.empty?
+      name_words.pop
+      name_words.push(response_b[rand(result_length(response_b.length))]["word"].capitalize.singularize)
+    else
+      name_words << "thing".capitalize
+    end
+      name_words.join(" ")
+
+  end
+
+  def result_length(lng)
+    if lng < 10
+      return lng
+    else
+      return (lng / 3).to_i
+    end
+
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :ingredients, only: [:index, :create, :update]
+      get 'cocktails/generate', to: 'cocktails#generate'
       resources :cocktails, except: [:new, :edit] #Full CRUD!!!
       resources :users, except: [:new, :edit]
-      
+
     end
 
   end
