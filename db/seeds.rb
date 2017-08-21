d8ebe476179a26a11e12b24b0bec3004494dbfe7
jeremylenz <jeremylenz@gmail.com>
2017-08-21 09:27:12 -0400

    guess alcohol content

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,25 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
-# Ingredient list courtesy of this dope API we found
-
-Ingredient.all.each do |ing|
-  ing.destroy
-end
-
 require 'HTTParty'
 require 'byebug'
 response = HTTParty.get('http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
 ingredients = JSON.parse(response.body)
 
+def guess_alcohol_content(ing_name)
+  is_alcoholic = false
+  alcohol_indicators = ["rum", "vermouth", "schnapps", "scotch", "brandy", "whiskey", "whisky", "blanc", "champagne", "liqueur", "cachaca", "brandy", "sherry", "shartreuse", "cognac", "jack", "sake", "creme", "cuervo", "tequila", "gin", "wine", "sambuca", "bacardi", "bailey's"]
+  is_alcoholic
+end
+
+# Ingredient list thanks to cocktaildb API
+
+Ingredient.all.each do |ing|
+  ing.destroy
+end
+
+
 ingredients["drinks"].each do |ing|
   img_url = "http://www.thecocktaildb.com/images/ingredients/#{ing["strIngredient1"]}.png"
-  Ingredient.create(name: ing["strIngredient1"], is_alcoholic: true, image_url: img_url)
+  Ingredient.create(name: ing["strIngredient1"], is_alcoholic: guess_alcohol_content(ing["strIngredient1"]), image_url: img_url)
 end
