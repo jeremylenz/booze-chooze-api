12ec3367a5177a69f5702e7b6696adb962b53876
jeremylenz <jeremylenz@gmail.com>
2017-08-21 11:36:54 -0400

    seeds updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,17 @@ require 'byebug'
 response = HTTParty.get('http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
 ingredients = JSON.parse(response.body)
 
-def guess_alcohol_content(ing_name)
-  is_alcoholic = false
-  alcohol_indicators = ["rum", "vermouth", "schnapps", "scotch", "brandy", "whiskey", "whisky", "blanc", "champagne", "liqueur", "cachaca", "brandy", "sherry", "shartreuse", "cognac", "jack", "sake", "creme", "cuervo", "tequila", "gin", "wine", "sambuca", "bacardi", "bailey's"]
-  is_alcoholic
-end
+NON_ALCOHOLIC_INGREDIENTS = ["Sugar", "Ice", "Lemon", "Lime juice", "Egg", "Mint", "Sweet and sour", "Salt", "Carbonated water", "Lemon peel", "Grenadine", "Grapefruit juice", "Vanilla ice-cream", "Lime juice cordial", "Apple juice", "Berries", "Blueberries", "Orange juice", "Cranberries", "Brown sugar", "Milk", "Mango", "Egg yolk", "Lemon juice", "Soda water", "Cream", "Pineapple", "Sugar syrup", "Ginger ale", "Worcestershire sauce", "Ginger", "Strawberries", "Chocolate syrup", "Yoghurt", "Grape juice", "Orange", "Apple cider", "Banana", "Soy milk", "Lime", "Cantaloupe", "Grapes", "Kiwi", "Tomato juice", "Cocoa powder", "Chocolate", "Heavy cream", "Coffee", "Water", "Espresso", "Honey", "Whipping cream", "Half-and-half", "Bread", "Plums", "Vanilla", "Apple", "Kool-Aid", "Lemonade", "Cranberry juice", "Eggnog", "Carbonated soft drink", "Cloves", "Raisins", "Almond", "Pink lemonade", "Sherbet", "Peach nectar", "Coca-Cola", "Tang", "Cider", "7-Up", "Limeade", "Maraschino cherry juice", "Cardamom", "Tabasco sauce", "Fruit punch", "Cherry juice", "Orange peel", "Clamato juice", "Sour mix", "Root beer", "Hot chocolate", "Jello", "Mountain Dew", "Cream of coconut", "Fruit juice", "Chocolate ice-cream", "Hawaiian Punch", "Tea", "demerara Sugar", "Passion fruit juice", "Pineapple juice", "Angelica root", "Condensed milk", "Tropicana", "Sprite", "Chocolate milk", "Beef bouillon", "Dr. Pepper", "Iced tea", "Club soda", "Cherry Cola", "Orange soda", "Lemon-lime soda", "Schweppes Russchian", "coconut milk"]
+
+# def guess_alcohol_content(ing_name)
+#   is_alcoholic = false
+#   alcohol_indicators = ["rum", "vermouth", "schnapps", "scotch", "brandy", "whiskey", "whisky", "blanc", "champagne", "liqueur", "cachaca", "brandy", "sherry", "shartreuse", "cognac", "jack", "sake", "creme", "cuervo", "tequila", "gin", "wine", "sambuca", "bacardi", "bailey's"]
+#   alcohol_indicators.each do |alcohol|
+#     if ing_name.downcase.include(alcohol)
+#       is_alcoholic = true
+#     end
+#   is_alcoholic
+# end
 
 # Ingredient list thanks to cocktaildb API
 
@@ -25,8 +31,9 @@ end
 
 
 ingredients["drinks"].each do |ing|
-  img_url = "http://www.thecocktaildb.com/images/ingredients/#{ing["strIngredient1"]}.png"
-  Ingredient.create(name: ing["strIngredient1"], is_alcoholic: guess_alcohol_content(ing["strIngredient1"]), image_url: img_url)
+  ing_name = ing["strIngredient1"]
+  img_url = "http://www.thecocktaildb.com/images/ingredients/#{ing_name}.png"
+  Ingredient.create(name: ing_name, is_alcoholic: !NON_ALCOHOLIC_INGREDIENTS.include?(ing_name), image_url: img_url)
 end
 
 puts "Database seeded with #{Ingredient.all.length} ingredients"
